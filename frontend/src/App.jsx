9520bf2d9f8eb4ffb8302200c197de17ff47dde4
Jeet2202 <jeet.a.gharat@gmail.com>
2026-03-11 02:54:26 +0530

    tsx appp jsx signup login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/common/LandingPage';
-import Login from './pages/common/Login';
-import Signup from './pages/common/Signup';
+import AnimatedLoginPage from './pages/animated-characters-login-page';
+import AnimatedSignupPage from './pages/AnimatedSignupPage';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import OrganizerDashboard from './pages/organizer/OrganizerDashboard';
 import CreateHackathon from './pages/organizer/CreateHackathon';
@@ -12,16 +12,17 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"                         element={<LandingPage />} />
-        <Route path="/login"                    element={<Login />} />
-        <Route path="/signup"                   element={<Signup />} />
-        <Route path="/admin-dashboard"          element={<AdminDashboard />} />
-        <Route path="/organizer-dashboard"      element={<OrganizerDashboard />} />
-        <Route path="/organizer/create"         element={<CreateHackathon />} />
-        <Route path="/organizer/manage"         element={<ManageHackathon />} />
-        <Route path="/organizer/event/:id"      element={<EventManagement />} />
-        <Route path="/organizer/events"         element={<EventManagement />} />
+        <Route path="/"                    element={<LandingPage />} />
+        <Route path="/login"               element={<AnimatedLoginPage />} />
+        <Route path="/signup"              element={<AnimatedSignupPage />} />
+        <Route path="/admin-dashboard"     element={<AdminDashboard />} />
+        <Route path="/organizer-dashboard" element={<OrganizerDashboard />} />
+        <Route path="/organizer/create"    element={<CreateHackathon />} />
+        <Route path="/organizer/manage"    element={<ManageHackathon />} />
+        <Route path="/organizer/event/:id" element={<EventManagement />} />
+        <Route path="/organizer/events"    element={<EventManagement />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
