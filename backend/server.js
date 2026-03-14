06b3dbb07377fc0fcb6c179f8bf8e0b194bd6055
Jeet2202 <jeet.a.gharat@gmail.com>
2026-03-14 07:54:36 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -59,4 +59,9 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+const HOST = process.env.RENDER_EXTERNAL_URL || `http://localhost:${PORT}`;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`\n🚀 Server running on: ${HOST}`);
+  console.log(`📡 Listening on port: ${PORT}`);
+
+});
